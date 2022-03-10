ee1a4f5adf317e305b908ff68b8c9f1dd041cc70
Your Name <you@example.com>
2022-03-10 15:06:58 +0800

    项目通用配置

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,6 @@ export default {
 </script>
 
 <style>
+/* 注意普通的import用于引用组件和js文件，@import用于引入css文件 */
+ @import "assets/css/base.css";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
-
+  configureWebpack: {
+    resolve:{
+      extensions: [".js", ".vue", ".json"],
+      // 别名
+      alias: {
+        assets:'@/assets',
+        common:'@/common',
+        components:'@/components',
+        network:'@/network',
+      }
+    }
+  }
 }
 
