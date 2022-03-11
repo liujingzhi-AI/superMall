6a3f29d918d0df69171c035158264926267febf9
Your Name <you@example.com>
2022-03-11 16:20:37 +0800

    顶部导航栏的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    首页
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar';
 export default {
   name: 'Home',
-
+  components:{
+    NavBar
+  },
   data() {
     return {
       
@@ -17,5 +20,9 @@ export default {
 </script>
 
 <style scoped>
-
+.home-nav{
+  /* 这是base.css中定义的颜色 */
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
