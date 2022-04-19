c40d771febe5e713584106a688e70984ab320a19
Your Name <you@example.com>
2022-04-19 16:51:00 +0800

    吸顶封装/本周流行

---
+++ src/views/home/childComponts/FeatureView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="feature">
+      <a href="https://act.mogujie.com/zzlx67">
+        <img src="@/assets/img/Home/recommend_bg.jpg" alt="" class="imgsty">
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Featureview",
+  props: {
+    keywords: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.headline {
+  color: black;
+  text-align: center;
+  margin-top: 10px;
+}
+.imgsty {
+  width: 100%;
+  height: 300px;
+}
+</style>
