ab9a393187b9add17db090b54f238d9b54411e79
Your Name <you@example.com>
2022-07-01 16:10:38 +0800

    购物车商品是否选中，checked在Vuex响应改变

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,8 @@
 // 作为函数的时候加中括号了
 import {
   ADD_COUNTER,
-  ADD_TO_CART
+  ADD_TO_CART,
+  CHECK_CHAN
 } from '@/store/mutation-type'
 
 export default {
@@ -11,8 +12,16 @@ export default {
   },
   // 加入到购物车
   [ADD_TO_CART](state,payload) {
-    payload.checked = false
+    payload.checked = true
     payload.count = 1
     state.cartList.push(payload)
+  },
+  // checked状态改变
+  [CHECK_CHAN](state,payload) {
+    state.cartList.forEach(item => {
+      if(item.iid == payload.iid){
+        item.checked = payload.checked
+      }
+    });
   }
 }
